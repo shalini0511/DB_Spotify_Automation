d5bf596e0701d6595f94f1a41f96488f0affe75f
shalini <shalini.v@tvsnext.io>
2021-08-23 15:28:25 +0530

    [shalini]Spotify_DB_Automation

---
+++ DB_Spotify/SpotifyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_Spotify
+{
+    public class SpotifyModel
+    {
+       
+            public int TrackId { get; set; }
+            public int PlaylistId { get; set; }
+            public string TrackName { get; set; }
+            public string ArtistName { get; set; }
+        
+    }
+}
+
---
+++ DB_Spotify/UnitTest1.cs
@@ -0,0 +1,76 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DB_Spotify
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            SpotifyAutomation model;
+
+            [TestInitialize]
+            public void setup()
+            {
+                model = new SpotifyAutomation();
+
+            }
+            [DataRow(5, "track9", "QWE")]
+            [DataRow(2, "track9", "RTY")]
+            [DataRow(3, "track9", "UIO")]
+            [DataRow(4, "track2", "PAS")]
+            [DataRow(1, "track5", "HJL")]
+            [DataRow(3, "track4", "ZXC")]
+            [DataTestMethod]
+            [Priority(1)]
+            [TestMethod]
+            public void TestMethod1ForInsert(int a, String b, String c)
+            {
+
+                int expected = 1;
+                SpotifyModel model1 = new SpotifyModel();
+                var actual = model.InsertIntoTable(a, b, c);
+                Assert.AreEqual(expected, actual);
+            }
+
+
+
+            [Priority(2)]
+            [TestMethod]
+            public void TestMethod2ForRetrive()
+            {
+                int expected = 11;
+                var actual = model.retriveData();
+                Assert.AreEqual(expected, actual);
+            }
+
+
+
+            [Priority(3)]
+            [TestMethod]
+            public void TestMethod3ForUpdate()
+            {
+                int expected = 1;
+                SpotifyModel model1 = new SpotifyModel();
+                var actual = model.EditExistingContact(model1);
+                Assert.AreEqual(expected, actual);
+            }
+
+
+
+            [Priority(4)]
+            [TestMethod]
+            public void TestMethod4ForDelete()
+            {
+                int expected = 1;
+                SpotifyModel model2 = new SpotifyModel();
+                var actual = model.Delete(model2);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+
+
+
+
+
+
